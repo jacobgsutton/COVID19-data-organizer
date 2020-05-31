4ec47236bbf8ffc6a6367c8117655551a7d2000f
BuildTools <unconfigured@null.spigotmc.org>
2020-05-31 01:12:45 -0400

    added dictionary for all fetched data

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#in development
+
 import datetime
 import io
 import requests
 
-#dictionary for florida's data specifically
-florida_data_dict = {}
+#dictionary for all states with data from the entire loaded file
+us_data_dict = {}
 
 #get date info needed
 today = datetime.date.today()
@@ -28,18 +30,47 @@ except requests.HTTPError as e:
 #get text from request       
 data = response.text
 
-#grabbing some data and putting it in lists
+#function to create a list of all the province/states in the data set
+def createProvStateList():
+    l = []
+    i = 0
+    for d in data:
+        if i < len(data)-1 and d == '\n':
+            l.append(data[i+1 : data.index(',', i)])
+        i+=1
+    return l
+
+#grabbing some data and putting it in lists (setup for generating dicts)
 data_categories = data[0 :  data.index('\n')].split(',')
-florida_data = data[data.index('Florida') : data.index('\n', data.index('Florida'))].split(',')
+data_categories_2 = data[data.index(',')+1 :  data.index('\n')].split(',')
+province_state_list = createProvStateList()
 
-#set up florida dict
-for key in data_categories:
-    for val in florida_data:
-        florida_data_dict[key] = val
-        florida_data.remove(val)
-        break
+#generates a dictory with all data from the file
+def generateUSDict():
+    i = 0
+    us_data = []
 
+    for state in province_state_list:
+        us_data.append(data[data.index(state)+len(state)+1 : data.index('\n', data.index(state))].split(','))
+
+    for key in province_state_list:
+        us_data_dict[key] = {}
+        for cat in data_categories_2:
+            for val in us_data[i]:
+                us_data_dict[key][cat] = val
+                us_data[i].remove(val)
+                break
+        i += 1
+
+
+#calls the fuction
+generateUSDict()
+
+#creates a var for florida dict
+florida_data_dict = us_data_dict['Florida']
 
 #testing prints
-print(florida_data_dict)
-print(florida_data_dict['Confirmed'])
+print(florida_data_dict['Confirmed'], '\n\n')
+
+for state in province_state_list:
+    print(state, ':', us_data_dict[state], '\n')
